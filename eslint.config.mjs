2ed68e2ed5ee05207d26c9c3e1bb887616a89024
Cauê Prado <caue.prado0@gmail.com>
2026-03-10 20:03:04 -0300

    chore: update ESLint configuration and dependencies for improved linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,37 @@
 import { defineConfig } from "eslint/config";
+import next from "@next/eslint-plugin-next";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
 
-export default defineConfig({
-  root: true,
-  ignorePatterns: [
-    "node_modules/**",
-    "build/**",
-    "out/**",
-    ".next/**",
-    "next-env.d.ts",
-  ],
-  extends: [
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
-  ],
-  settings: {
-    react: {
-      version: "detect",
+export default defineConfig([
+  {
+    ignores: [
+      "node_modules/**",
+      "build/**",
+      "out/**",
+      ".next/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    plugins: {
+      next,
+      react,
+      "react-hooks": reactHooks,
+      prettier,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...next.configs["core-web-vitals"].rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
     },
   },
-});
+]);
